568b5fae7b85b608db09ee5bd4a8005be4a2f5c4
hibbes <marek@czernohous.de>
2016-10-18 11:03:14 +0200

    Weitere ASV-Anpassungen

--- RaspiAnm/eintragen.php
+++ RaspiAnm/eintragen.php
@@ -2,20 +2,22 @@
 include("dbconnect.php");
 
 $eintrags_id = $_POST["id"];
+
+$name = $_POST["name"];
 $vorname = $_POST["vorname"];
-$zweitname = $_POST["zweitname"];
-$nachname = $_POST["zuname"];
+$rufname = $_POST["rufname"];
 $geschlecht = $_POST["geschlecht"];
-$geburtsdatum = $_POST["geburtsdatum"];
+$geburtstag = $_POST["geburtsdatum"];
 $geburtsort = $_POST["geburtsort"];
 $geburtsland = $_POST["geburtsland"];
-$staat1 = $_POST["staat1"];
-$staat2 = $_POST["staat2"];
+$land = $_POST["staat1"];
+$land2 = $_POST["staat2"];
+$muttersprache = $_POST["muttersprache"];
 $bekenntnis = $_POST["bekenntnis"];
 $abgebendeschule = $_POST["schule"];
-$klasse = $_POST["klasse"];
+
 $sprachwahl = $_POST["sprachwahl"];
-$reli = $_POST["reli"];
+$ru = $_POST["reli"];
 $zug = $_POST["zug"];
 $strasse1 = $_POST["strasse1"];
 $hausnummer1 = $_POST["hausnummer1"];
@@ -24,22 +26,32 @@ $plz1 = $_POST["plz1"];
 $erz1vorname = $_POST["Erz1Vorname"];
 $erz1name = $_POST["Erz1Name"];
 $erz1geschlecht = $_POST['erz1geschlecht'];
+$erz1strasse = $_POST["erz1strasse"];
+$erz1hausnummer = $_POST["erz1hausnummer"];
+$erz1ort = $_POST["erz1ort"];
+$erz1plz = $_POST["erz1plz"];
+$erz1telefon = $_POST["erz1telefon"];
+$erz1telefon2 = $_POST["erz1telefon2"];
+$erz1email = $_POST["erz1email"];
+$erz1handy = $_POST["erz1handy"];
+
+
 $erz2vorname = $_POST["Erz2Vorname"];
 $erz2name = $_POST["Erz2Name"];
 $erz2geschlecht = $_POST['erz2geschlecht'];
-$telefon1 = $_POST["telefon1"];
-$email1 = $_POST["email1"];
-$mobil1 = $_POST["mobil1"];
+
+
 $erz2vorname = $_POST["Erz2Vorname"];
 $erz2name = $_POST["Erz2Name"];
-$strasse2 = $_POST["strasse2"];
-$hausnummer2 = $_POST["hausnummer2"];
-$ort2 = $_POST["ort2"];
-$plz2 = $_POST["plz2"];
-$telefon2 = $_POST["telefon2"];
-$email2 = $_POST["email2"];
-$mobil2 = $_POST["mobil2"];
-$aussiedler = $_POST["aussiedler"];
+$erz2strasse = $_POST["erz2strasse"];
+$erz2hausnummer = $_POST["erz2hausnummer"];
+$erz2ort = $_POST["erz2ort"];
+$erz2plz = $_POST["erz2plz"];
+$erz2telefon = $_POST["erz2telefon"];
+$erz2telefon2 = $_POST["erz2telefon2"];
+$erz2email = $_POST["email2"];
+$erz2handy = $_POST["mobil2"];
+
 
 $foto = $_POST["foto"];
 $notfallnr = $_POST["notfallnr"];
@@ -56,16 +68,16 @@ $ergebnis = mysqli_query($db, $abfrage);
 echo "Formular-ID ist $eintrags_id";
 
 if(mysqli_num_rows($ergebnis)==0){
-	$eintrag = "INSERT INTO Anmeldungen (Vorname, Rufname, Nachname, Geschlecht, Geburtstag, Strasse, HausNr, PLZ, Ort, 
-	Geburtsort, Geburtsland, Land, Land2, Religion, AbgebendeSchule, Klasse, Sprachwahl, 
-	RU, Profil1, Strasse, HausNr, PLZ, Ort, Erz1Name, Erz1Vorname, Erz1Geschlecht, Erz2Vorname, Erz2Name, Erz2Geschlecht,
-	Erz1Telefon, Erz1Email, Erz1Handy, Foto_Einw, Notfallnr, Geschwisterklasse, Klassenpartner, 
-	nichtKlassenpartner, Erz2Strasse, Erz2PLZ, Erz2Ort, 
-	Erz2Telefon, Erz2Handy, Erz2Email, Aussiedler, Eintrags_ID) VALUES
-	('$vorname', '$zweitname', '$nachname', '$geschlecht', '$geburtsdatum', '$strasse1', '$hausnummer1', '$plz1', '$ort1, '$geburtsort', 
-	'$geburtsland', '$staat1', '$staat2', '$bekenntnis', '$abgebendeschule', '$klasse', '$sprachwahl', '$reli',
-	'$zug', '$strasse1', '$hausnummer1', $plz1', '$ort1', '$erz1name', '$erz1vorname', '$erz1geschlecht', '$erz2vorname', '$erz2name', '$erz2geschlecht', '$telefon1',
-	'$email1', '$mobil1', '$foto', '$notfallnr', '$geschwister', '$mit', '$ohne', '$strasse2', '$plz2', '$ort2', '$telefon2', '$mobil2', '$email2', '$aussiedler', $eintrags_id')";
+	$eintrag = "INSERT INTO Anmeldungen (Eintrags_ID, Klasse, Name, Vorname, Rufname, Geschlecht, Geburtstag, Geburtsort, Geburtsland, Geschlecht, Religion, RU, Land, ,Land2, Strasse, HausNr, PLZ, Ort, 
+	Muttersprache, Erz1Name, Erz1Vorname, Erz1Geschlecht, Erz1Strasse, Erz1HausNr, Erz1PLZ, Erz1Ort,
+	Erz1Telefon, Erz1Telefon2 , Erz1Handy, Erz1Email, Erz2Vorname, Erz2Name, Erz2Geschlecht, Erz1Strasse, Erz1HausNr, Erz1PLZ, Erz1Ort,
+	Erz2Telefon, Erz2Telefon2, Erz2Handy, Erz2Email, AbgebendeSchule, Sprachwahl, Foto_Einw, Notfallnr, Geschwister, Klassenpartner, 
+	nichtKlassenpartner) VALUES
+	('$eintrags_id', '5', '$name', '$vorname', '$rufname', $geburtstag', '$geburtsort', '$geburtsland', '$geschlecht', '$religion', '$ru', '$land', '$land2', '$strasse', '$hausnummer', '$plz', '$ort,  
+	 '$muttersprache',  '$erz1name' '$erz1vorname', '$erz1geschlecht' , '$erz1strasse', '$erz1hausnummer' ,
+	 '$erz1plz', '$erz1ort', '$erz1telefon, '$erz1telefon2', '$erz1handy', $erz1email',
+	 '$erz2vorname', '$erz2name', '$erz2geschlecht', '$erz2strasse', '$erz2hausnummer', '$erz2plz', '$erz2ort', '$erz2telefon', '$erz2telefon2', '$erz2handy', '$erz2email' 
+	 '$abgebendeschule', '$sprachwahl', '$foto', '$notfallnr', '$geschwister', '$mit', '$ohne')";
 	
 	$eintragen = mysqli_query($db, $eintrag);
 	echo "<br> Datenbankeintrag erfolgreich!";
