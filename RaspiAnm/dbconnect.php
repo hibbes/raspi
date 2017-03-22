198f58d8a11f41c6f0552eb96c939e9717fff9cb
hibbes <marek@czernohous.de>
2017-03-22 08:25:37 +0100

    UTF-8 Zeichenkodierung im Header hinzugefügt

--- RaspiAnm/dbconnect.php
+++ RaspiAnm/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-
+header("Content-Type: text/html; charset=utf-8");
 $db = mysqli_connect("localhost", "anmeldungen", "seki", "anmeldungen");
-$db -> set_charset("utf8");
+$db -> set_charset('utf8');
 if(!$db)
 {
 	exit("Verbindungsfehler: ".mysqli_connect_error());
